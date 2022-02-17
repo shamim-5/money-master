ee8c9390f5559c905692be8f4ae39009b529371c
mss38 <email@example.com>
2022-02-17 15:58:27 +0600

    Money Master project completed

--- js/app.js
+++ js/app.js
@@ -1,38 +1,39 @@
-//calculate total cost
-function getTotalCost() {
-  const foodCost = getAmountSingle("food");
-  const rentCost = getAmountSingle("rent");
-  const clothsCost = getAmountSingle("clothes");
-  const costTotalAmount = foodCost + rentCost + clothsCost;
-  const expensesTotal = document.getElementById("expenses-total");
-
-  expensesTotal.innerText = costTotalAmount;
-  return costTotalAmount;
-  console.log(costTotalAmount);
-}
-
 // common function
 function getAmountSingle(amount) {
   const itemCost = document.getElementById(amount + "-cost");
   const itemCostAmount = parseFloat(itemCost.value);
-  return itemCostAmount;
+
+  if (itemCostAmount > 0) {
+    return itemCostAmount;
+  } else if (isNaN()) {
+    alert("Please put valid number !");
+  }
+}
+function getTotal(amount) {
+  const total = document.getElementById(amount + "-total");
+  const totalAmount = parseFloat(total.value);
+
+  if (totalAmount > 0) {
+    return totalAmount;
+  } else if (isNaN()) {
+    alert("Please put only number !");
+  }
 }
 function getAmountTotal(amount) {
   const catchAmount = document.getElementById(amount + "-total");
   const totalAmount = parseFloat(catchAmount.innerText);
   return totalAmount;
 }
-function getTotal(amount) {
-  const total = document.getElementById(amount + "-total");
-  const totalAmount = parseFloat(total.value);
-  return totalAmount;
+// calculate total cost
+function getTotalCost() {
+  const foodCost = getAmountSingle("food");
+  const rentCost = getAmountSingle("rent");
+  const clothsCost = getAmountSingle("clothes");
+  const costTotalAmount = foodCost + rentCost + clothsCost;
+  const expensesTotal = document.getElementById("expenses-total");
+  expensesTotal.innerText = costTotalAmount;
+  return costTotalAmount;
 }
-// calculate button event handler
-document.getElementById("calculate-btn").addEventListener("click", function () {
-  const balanceTotal = document.getElementById("balance-total");
-  balanceTotal.innerText = getTotal("income") - getTotalCost();
-  getTotalCost();
-});
 // calculate savings amount
 function getTotalSavings() {
   const percentTotalAmount = getTotal("percent") / 100;
@@ -41,6 +42,13 @@ function getTotalSavings() {
   savingsTotal.innerText = percentTotalAmount * incomeTotalAmount;
   return percentTotalAmount * incomeTotalAmount;
 }
+// calculate button event handler
+document.getElementById("calculate-btn").addEventListener("click", function () {
+  const balanceTotal = document.getElementById("balance-total");
+
+  balanceTotal.innerText = getTotal("income") - getTotalCost();
+  getTotalCost();
+});
 // save button event handler
 document.getElementById("savings-btn").addEventListener("click", function () {
   const savingsTotalAmount = getAmountTotal("savings");
